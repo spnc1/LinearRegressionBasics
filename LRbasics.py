6c7e7ad3ea39c5e2a29d0d433fba05c238a9c654
spnc1 <cooley.exe@gmail.com>
2024-01-23 18:15:40 +1100

    final version

--- LRbasics.py
+++ LRbasics.py
@@ -2,45 +2,46 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 filename = 'ExampleData.csv'
-OverflowPrevent = 1000
+OverflowPrevent = 1
+UpdateInterval = 50
 
 m = 0
-c = -5
+c = 0
 L = 0.0001
-#epochs = 50
+epochs = 1000
 
 GraphMin = 0
 GraphMax = 5
 
-X = []
-Y = []
-
 data = pd.read_csv("C:\\Users\\coole\\Documents\\AI\\LinearRegression\\TestData\\"+filename)
 
-def loss_function(m, c, points):
-    x_error = 0
-    y_error = 0
-    total_x_error = 0
-    total_y_error = 0
-    for i in range(len(points)):
-        x = points.iloc[i].IV
-        y = points.iloc[i].DV
+#L = Learning Rate
+def gradient_descent(m_now, c_now, points, L):
+    dedm = 0
+    dedc = 0
 
-        y_error = y - (m*x+c)
-        total_y_error += y_error*y_error
+    n = len(points)
 
-    X.append(c)
-    Y.append(total_y_error)
-    
+    for i in range(n):
+        x = points.iloc[i].IV/OverflowPrevent
+        y = points.iloc[i].DV/OverflowPrevent
 
-for i in range(15):
-    loss_function(m,c,data)
-    c+=1
+        dedm += -(2/n) * x * (y - (m_now * x + c_now))
+        dedc += -(2/n) * (y - (m_now * x + c_now))
 
+    m = m_now - dedm * L
+    c = c_now - dedc * L
 
+    return m,c
 
-#plt.scatter(data.IV,data.DV)
-#plt.plot(list(range(GraphMin,GraphMax+1)), [m * x + c for x in range(GraphMin,GraphMax+1)], color="red")
+for i in range(epochs):
+    if i % UpdateInterval == 0:
+        print(f'Epoch: {i}')
+    m, c = gradient_descent(m, c, data, L)
 
-plt.scatter(X,Y)
+print(m, c)
+
+plt.scatter(data.IV,data.DV)
+plt.plot(list(range(GraphMin,GraphMax+1)), [m * x + c for x in range(GraphMin,GraphMax+1)], color="red")
+
 plt.show()
